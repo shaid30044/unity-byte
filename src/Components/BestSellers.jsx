a7057dcdc6a4953cfcc906db89882aa1a68c6d16
shaid30044 <kaona30044@gmail.com>
2023-12-28 21:13:32 +0600

    add data on Best Sellers section

--- src/Components/BestSellers.jsx
+++ src/Components/BestSellers.jsx
@@ -30,6 +30,61 @@ const BestSellers = () => {
           BEST SELLERS
         </span>
       </div>
+
+      <div>
+        {products.map((product, idx) => (
+          <div key={idx} className="flex gap-6">
+            {/* image */}
+
+            <div className="flex justify-center items-center w-3/12">
+              <img src={product.image} alt={product.title} />
+            </div>
+
+            <div className="w-9/12">
+              {/* rating */}
+
+              <div>
+                <Rating
+                  style={{ maxWidth: 100 }}
+                  readOnly
+                  value={product.rating}
+                  itemStyles={myStyles}
+                />
+              </div>
+
+              {/* title */}
+
+              <h3 className="sm:text-lg font-semibold text-gray-600 hover:text-orange-500 duration-200 cursor-pointer pt-1">
+                {product.title.length >= 30 ? (
+                  <p>{product.title.slice(0, 30)}...</p>
+                ) : (
+                  <p>{product.title}</p>
+                )}
+              </h3>
+
+              {/* price */}
+
+              <div className="pt-2">
+                {product.new_price ? (
+                  <div className="flex items-end gap-4">
+                    <p className="text-xl sm:text-2xl font-bold text-orange-500">
+                      ${product.new_price}
+                    </p>
+
+                    <p className="text-lg font-bold text-gray-400 line-through">
+                      ${product.old_price}
+                    </p>
+                  </div>
+                ) : (
+                  <p className="text-2xl font-bold text-orange-500">
+                    ${product.old_price}
+                  </p>
+                )}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
